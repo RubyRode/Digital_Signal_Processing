f283a0368d1540c019440f51b4adce22708084a0
RubyRode <dimaskrut71@gmail.com>
2023-10-01 11:37:53 +0700

    Fixed some variables

--- lab_2.py
+++ lab_2.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math
-from scipy.fft import fft, ifft, fftfreq, fft
+from scipy.fft import ifft, fftfreq, fft
 from scipy.signal import square
 from timeit import default_timer as timer
 
@@ -19,27 +18,27 @@ def cosine(lp, frequency):
 def dft_slow(x):
     """Compute the discrete Fourier Transform of the 1D array x"""
     x = np.asarray(x, dtype=float)
-    N = x.shape[0]
-    n = np.arange(N)
-    k = n.reshape((N, 1))
-    M = np.exp(-2j * np.pi * k * n / N)
-    return np.dot(M, x)
+    n = x.shape[0]
+    r = np.arange(n)
+    k = r.reshape((n, 1))
+    m = np.exp(-2j * np.pi * k * r / n)
+    return np.dot(m, x)
 
 
 def my_fft(x):
     x = np.asarray(x)  # Convert input to numpy array
-    N = x.shape[0]
-    if N <= 1:
+    n = x.shape[0]
+    if n <= 1:
         return x
 
     even = my_fft(x[::2])  # Recursive call for evens
     odd = my_fft(x[1::2])  # Recursive call for odds
 
     # Calculate the FFT
-    T = [np.exp(-2j * np.pi * k / N) * odd[k] for k in range(N // 2)]
+    t = [np.exp(-2j * np.pi * k / n) * odd[k] for k in range(n // 2)]
 
     # Combine the even and odd parts to get the full spectrum
-    spectrum = [even[k] + T[k] for k in range(N // 2)] + [even[k] - T[k] for k in range(N // 2)]
+    spectrum = [even[k] + t[k] for k in range(n // 2)] + [even[k] - t[k] for k in range(n // 2)]
 
     return np.array(spectrum)
 
@@ -78,24 +77,32 @@ class Solution:
                 i.grid(1)
         axs[0, 0].set_title('Original signal')
         axs[0, 0].plot(self.lp, self.wv_sum)
+
         axs[1, 0].set_title('FFT of original signal')
         axs[1, 0].plot(lps, ff)
+
         axs[1, 1].set_title('DFT_slow of original signal')
         axs[1, 1].plot(lps, dft)
+
         axs[0, 1].set_title('Time comparison between fft and dft')
         axs[0, 1].bar([1, 2, 3], height=np.array(ht), width=0.2,
                       align='center', color=['b', 'g', 'r'], edgecolor='black',
                       linewidth=3, tick_label=['fft time', 'dft time', 'difference'])
+
         axs[2, 0].set_title('IFFT of FFT')
         axs[2, 0].plot(lps, ifft_ff)
+
         axs[2, 1].set_title('IFFT of DFT_slow')
         axs[2, 1].plot(lps, ifft_dft)
+
         axs[3, 0].plot(lps, np.abs(ifft_ff - self.wv_sum))
         axs[3, 0].set_title('Error original signal vs ifft_fft')
         axs[3, 0].set_ylim([-1, 1])
+
         axs[3, 1].plot(lps, np.abs(ifft_dft - self.wv_sum))
         axs[3, 1].set_title('Error original signal vs ifft_dft_slow')
         axs[3, 1].set_ylim([-1, 1])
+
         plt.show()
         noisy = self.wv_sum + self.noise_50150
         fft_noisy = fft(noisy)
@@ -107,10 +114,13 @@ class Solution:
 
         axs[0, 0].set_title('Original signal')
         axs[0, 0].plot(lps, self.wv_sum)
+
         axs[0, 1].set_title('Noisy original signal')
         axs[0, 1].plot(lps, noisy)
+
         axs[1, 0].set_title('fft of noisy signal')
         axs[1, 0].plot(lps, fft_noisy)
+
         axs[1, 1].set_title('ifft of fft_noisy')
         axs[1, 1].plot(lps, ifft(fft_noisy))
 
@@ -135,23 +145,30 @@ class Solution:
         axs[0, 0].set_title("Original Square signal")
         axs[0, 0].set_xlim([-0.5, 4.5])
         axs[0, 0].set_ylim([-3, 3])
+
         axs[0, 1].plot(lp_4, noisy_sq_sig)
         axs[0, 1].set_title("Noisy Square signal")
         axs[0, 1].set_xlim([-0.5, 4.5])
         axs[0, 1].set_ylim([-6, 6])
+
         axs[1, 0].set_title("Discrete spectrum FFT")
         axs[1, 0].plot(lp_4, sq_fft)
         axs[1, 0].set_xlim([-0.5, 4.5])
         axs[1, 0].set_ylim([-5, 14])
+
         axs[2, 0].set_title("Discrete spectrum DFT_Slow")
         axs[2, 0].plot(dft_sq_sig)
+
         axs[1, 1].set_title("Discrete spectrum FFT noisy")
         axs[1, 1].plot(lp_4, noisy_sq_fft)
+
         axs[2, 1].set_title("Discrete spectrun DFT noisy")
         axs[2, 1].plot(lp_4, dft_noisy_sq_sig)
+
         plt.show()
 
-    def task_3(self):
+    @staticmethod
+    def task_3():
         lp_tmp = np.linspace(0, 5, 4096)
         secs = np.abs(lp_tmp.min()) + np.abs(lp_tmp.max())
         cosine_wv = np.cos(2 * np.pi * 50 * lp_tmp)
@@ -200,9 +217,9 @@ LENGTH = 4 * np.pi
 SAMPLES = 1000
 AMPLITUDE = 2
 FREQUENCY = 0.5
-lps = np.linspace(-LENGTH // 2, LENGTH // 2, SAMPLES)
+lps_ = np.linspace(-LENGTH // 2, LENGTH // 2, SAMPLES)
 
-m = Solution(FREQUENCY, lps, AMPLITUDE)
-m.task_1()
-m.task_2()
-m.task_3()
+s = Solution(FREQUENCY, lps_, AMPLITUDE)
+s.task_1()
+s.task_2()
+s.task_3()
