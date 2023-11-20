50c90d826ead1f321afebd8bb78c0d6305ca0463
RubyRode <dimaskrut71@gmail.com>
2023-11-20 09:53:19 +0700

    lab_7

--- lab_7.py
+++ lab_7.py
@@ -1,24 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cmath
 from scipy.fft import fft, ifft, fftfreq
 from scipy.signal import sawtooth, square
 from scipy.stats import norm
 
 
+def find_half_max(signal):
+    max_amplitude = np.max(signal)
+    half_max = max_amplitude / 2
+    indices = np.where(signal >= half_max)[0]
 
-def peak(x, c):
-    return np.exp(-np.power(x - c, 2) / 16.0)
+    first_index = indices[0]
+    last_index = indices[-1]
 
-def lin_interp(x, y, i, half):
-    return x[i] + (x[i+1] - x[i]) * ((half - y[i]) / (y[i+1] - y[i]))
+    if len(indices) % 2 == 0:
+        first_index -= 1
+        last_index += 1
 
-def half_max_x(x, y):
-    half = max(y)/2.0
-    signs = np.sign(np.add(y, -half))
-    zero_crossings = (signs[0:-2] != signs[1:-1])
-    zero_crossings_i = np.where(zero_crossings)[0]
-    return lin_interp(x, y, zero_crossings_i[0], half) - \
-           lin_interp(x, y, zero_crossings_i[1], half)
+    return last_index - first_index
+
 
 def run_mean(x, k=40):
     tmp_x = x.copy()
@@ -28,15 +29,30 @@ def run_mean(x, k=40):
             if k <= i and k <= len(x) - i:
                 s += x[i + j]
 
-        tmp_x[i] = (2 * k + 1)**-1 * s
+        tmp_x[i] = (2 * k + 1) ** -1 * s
         s = 0
 
     return tmp_x
 
 
-def g():
+def g(mw, t):
+    return np.exp((-4 * cmath.log(2, np.e) * t ** 2) / mw ** 2)
 
-def gaussian_smooth(x):
+
+def gaussian_smooth(y, k):
+    tmp_x = y.copy()
+    s = 0
+
+    mw = find_half_max(y)
+    for i in range(len(tmp_x)):
+        for j in range(-k, k):
+            if k <= i and k <= len(y) - i:
+                s += y[i + j] * g(mw, y[i + j])
+
+        tmp_x[i] = s
+        s = 0
+
+    return tmp_x
 
 
 def task1():
@@ -45,8 +61,30 @@ def task1():
 
     plt.plot(lp, sig)
     plt.plot(lp, run_mean(sig, k=50))
-    # plt.xlim([0, 0.5])
     plt.show()
 
 
-task1()
+def task2():
+    lp = np.linspace(0, 0.5, 10000)
+    sig = np.sin(lp * 2 * np.pi * 10) + np.random.default_rng(seed=42).normal(0, 0.5, len(lp))
+    k = 50
+    og = gaussian_smooth(sig, k=k)
+    rm = run_mean(sig, k=k)
+    plt.plot(lp, sig/sig.max())
+    plt.plot(lp, rm / (sig.max()/rm.max()))
+    plt.plot(lp, og / (og.max()/rm.max()), "k")
+    plt.show()
+
+
+def task3():
+    lp = np.linspace(0, 0.5, 1000)
+    sig = np.random.normal(loc=1, scale=0.1, size=lp.shape)
+    sig[sig < 0] = 0
+    # sig = np.array([x/x for x in sig])
+    plt.bar(lp, sig)
+    plt.show()
+
+
+# task1()
+# task2()
+task3()
